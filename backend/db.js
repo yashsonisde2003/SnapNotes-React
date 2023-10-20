38b0cbc2524d968adc1f977828b6bdd1d5fed3db
Yash soni <144239405+yashsonisde2003@users.noreply.github.com>
2023-10-21 01:52:31 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-let mongoURI = "mongodb://127.0.0.1:27017/inotebook";
+let mongoURI = "your_mongodb_connection_string";
 const connecttomongo = () => {
   mongoose.connect(mongoURI);
   console.log('connection successfull')
